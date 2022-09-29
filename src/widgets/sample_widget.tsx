3f990b11030944794508bca2f540fe8c1b7385c2
00x0101101 <36686082+00x0101101@users.noreply.github.com>
2022-09-29 21:08:25 +0800

    Implemented the reserved word "Partial" in CSharp

--- src/widgets/sample_widget.tsx
+++ src/widgets/sample_widget.tsx
@@ -1,21 +1,34 @@
-import { usePlugin, renderWidget, useTracker } from '@remnote/plugin-sdk';
+import {
+  renderWidget,
+  RemRichTextEditor,
+} from '@remnote/plugin-sdk';
+import React from 'react';
 
 export const SampleWidget = () => {
-  const plugin = usePlugin();
 
-  let name = useTracker(() => plugin.settings.getSetting<string>('name'));
-  let likesPizza = useTracker(() => plugin.settings.getSetting<boolean>('pizza'));
-  let favoriteNumber = useTracker(() => plugin.settings.getSetting<number>('favorite-number'));
+  // let name = useTracker(() => plugin.settings.getSetting<string>('name'));
+  // let likesPizza = useTracker(() => plugin.settings.getSetting<boolean>('pizza'));
+  // let favoriteNumber = useTracker(() => plugin.settings.getSetting<number>('favorite-number'));
 
   return (
-    <div className="p-2 m-2 rounded-lg rn-clr-background-light-positive rn-clr-content-positive">
-      <h1 className="text-xl">Sample Plugin</h1>
-      <div>
-        Hi {name}, you {!!likesPizza ? 'do' : "don't"} like pizza and your favorite number is{' '}
-        {favoriteNumber}!
+    <div>
+      {/*<div className="p-2 m-2 rounded-lg rn-clr-background-light-positive rn-clr-content-positive">*/}
+      {/*  <h1 className="text-xl">Sample Plugin</h1>*/}
+      {/*  <div>*/}
+      {/*    Hi {name}, you {!!likesPizza ? 'do' : "don't"} like pizza and your favorite number is{' '}*/}
+      {/*    {favoriteNumber}!*/}
+      {/*  </div>*/}
+      {/*</div>*/}
+      <RemRichTextEditor readOnly={false} remId='PMRDZB50sLPC9boHy' width='50%' height={50}/>
+      <div data-search-result="search-result-item" id="search-results__result" className="cursor-pointer rounded">
+
       </div>
     </div>
+
   );
 };
+
+
+
 
 renderWidget(SampleWidget);
